2b01481ec5ae7c9d8ddad43dd66e3a225e5a7a6a
cristianpb <felipebrokate@gmail.com>
2019-03-04 23:38:11 +0100

    Add pred method for camera

--- camera_opencv.py
+++ camera_opencv.py
@@ -1,9 +1,11 @@
 import cv2
+import time
+from datetime import datetime
 from base_camera import BaseCamera
 from ssd_detection import SSD
 ssd = SSD()
 
-class Camera(BaseCamera):
+class CameraPred(BaseCamera):
     video_source = 0
 
     @staticmethod
@@ -12,7 +14,6 @@ class Camera(BaseCamera):
 
     @staticmethod
     def frames():
-    #def frames(self):
         camera = cv2.VideoCapture(Camera.video_source)
         if not camera.isOpened():
             raise RuntimeError('Could not start camera.')
@@ -28,3 +29,39 @@ class Camera(BaseCamera):
 
             # encode as a jpeg image and return it
             yield cv2.imencode('.jpg', img)[1].tobytes()
+
+
+class Camera(BaseCamera):
+    video_source = 0
+
+    @staticmethod
+    def set_video_source(source):
+        Camera.video_source = source
+
+    @staticmethod
+    def frames():
+        camera = cv2.VideoCapture(Camera.video_source)
+        if not camera.isOpened():
+            raise RuntimeError('Could not start camera.')
+
+        while True:
+            # read current frame
+            _, img = camera.read()
+
+            # encode as a jpeg image and return it
+            yield cv2.imencode('.jpg', img)[1].tobytes()
+
+def CaptureContinous():
+    while(1):
+        print("Capturing")
+        cap = cv2.VideoCapture(0)
+        # Capture frame-by-frame
+        ret, image = cap.read()
+        output = ssd.prediction(image)
+        output = ssd.filter_prediction(output)
+        if len(output) > 0:
+            image = ssd.draw_boxes(image, output)
+            filename_output = "./static/imgs/{}.jpg".format(datetime.now().strftime("%Y%m%d_%H%M%S"))
+            cv2.imwrite(filename_output,image)
+        cap.release()
+        time.sleep(60)

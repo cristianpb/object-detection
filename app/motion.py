e34357a2d8cf2a5868c6a643a7577d6ae68d63c0
cristianpb <felipebrokate@gmail.com>
2019-05-25 22:29:39 +0200

    Add gaussian blur for motion filter

--- app/motion.py
+++ app/motion.py
@@ -20,6 +20,7 @@ class Detector():
     @timeit
     def prediction(self, image):
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.GaussianBlur(image, (21, 21), 0)
         if self.avg is None:
             self.avg = image.copy().astype(float)
         cv2.accumulateWeighted(image, self.avg, 0.5)
@@ -36,25 +37,26 @@ class Detector():
 
     @timeit
     def filter_prediction(self, output, image):
-        df = pd.DataFrame(output)
-        if df.empty:
-            return df
-        df = df.assign(
-                area=lambda x: df[0].apply(lambda x: cv2.contourArea(x)),
-                bounding=lambda x: df[0].apply(lambda x: cv2.boundingRect(x))
-                )
-        df = df[df['area'] > MIN_AREA]
-        df_filtered = pd.DataFrame(
-                df['bounding'].values.tolist(), columns=['x1', 'y1', 'w', 'h'])
-        df_filtered = df_filtered.assign(
-                x1=lambda x: x['x1'].clip(0),
-                y1=lambda x: x['y1'].clip(0),
-                x2=lambda x: (x['x1'] + x['w']),
-                y2=lambda x: (x['y1'] + x['h']),
-                label=lambda x: x.index.astype(str),
-                class_name=lambda x: x.index.astype(str),
-                )
-        return df_filtered
+        if len(output) < 2:
+            return pd.DataFrame()
+        else:
+            df = pd.DataFrame(output)
+            df = df.assign(
+                    area=lambda x: df[0].apply(lambda x: cv2.contourArea(x)),
+                    bounding=lambda x: df[0].apply(lambda x: cv2.boundingRect(x))
+                    )
+            df = df[df['area'] > MIN_AREA]
+            df_filtered = pd.DataFrame(
+                    df['bounding'].values.tolist(), columns=['x1', 'y1', 'w', 'h'])
+            df_filtered = df_filtered.assign(
+                    x1=lambda x: x['x1'].clip(0),
+                    y1=lambda x: x['y1'].clip(0),
+                    x2=lambda x: (x['x1'] + x['w']),
+                    y2=lambda x: (x['y1'] + x['h']),
+                    label=lambda x: x.index.astype(str),
+                    class_name=lambda x: x.index.astype(str),
+                    )
+            return df_filtered
 
     def draw_boxes(self, image, df):
         for idx, box in df.iterrows():
@@ -71,6 +73,7 @@ if __name__ == "__main__":
 
     image2 = cv2.imread("./imgs/pi/20190314/063538_person_.jpg")
     image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
+    image2 = cv2.GaussianBlur(image2, (21, 21), 0)
     detector.avg = image2.astype(float)
 
     output = detector.prediction(image)

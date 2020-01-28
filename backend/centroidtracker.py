7b3a21f457b0afa52ffc227f1d34f33506c0fe0e
cristianpb <felipebrokate@gmail.com>
2020-01-28 23:18:25 +0100

    Write to csv when registering new object

--- backend/centroidtracker.py
+++ backend/centroidtracker.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from scipy.spatial import distance as dist
 import numpy as np
 
@@ -22,6 +23,11 @@ class CentroidTracker():
         self.objects[self.nextObjectID] = centroid
         self.disappeared[self.nextObjectID] = 0
         self.nextObjectID += 1
+        day = datetime.now().strftime("%Y%m%d")
+        hour = datetime.now().strftime("%H%M%S")
+        myCsvRow = "{},{},{},{}\n".format(day, hour, list(self.objects.keys()),[x.tolist() for x in list(self.objects.values())])
+        with open('imgs/tracking.csv','a') as fd:
+            fd.write(myCsvRow)
 
     def deregister(self, objectID):
         # to deregister an object ID we delete the object ID from

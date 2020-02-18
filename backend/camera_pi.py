55441d9f97fb8dde899a22289dec2d9efc8efbc7
cristianpb <felipebrokate@gmail.com>
2020-02-18 19:49:16 +0100

    :wrench: Adapt object tracking algorithm to raspberry

--- backend/camera_pi.py
+++ backend/camera_pi.py
@@ -1,15 +1,20 @@
 import os
 import io
 import cv2
+import time
+import glob
 import base64
 import numpy as np
 from celery import Celery
+from functools import reduce
 from datetime import datetime, timedelta
 from importlib import import_module
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from dotenv import load_dotenv
+from backend.centroidtracker import CentroidTracker
 from backend.base_camera import BaseCamera
+from backend.utils import reduce_tracking
 
 load_dotenv()
 Detector = import_module('backend.' + os.environ['DETECTION_MODEL']).Detector
@@ -54,15 +59,35 @@ class Camera(BaseCamera):
                 stream.seek(0)
                 stream.truncate()
 
-    @staticmethod
-    def prediction(img):
-        output = detector.prediction(img)
-        df = detector.filter_prediction(output, img)
+
+class Predictor(object):
+    """Docstring for Predictor. """
+
+    def __init__(self):
+        self.detector = Detector()
+        self.ct = CentroidTracker(maxDisappeared=20)
+
+    def prediction(self, img, conf_th=0.3, conf_class=[]):
+        output = self.detector.prediction(img)
+        df = self.detector.filter_prediction(output, img, conf_th=conf_th, conf_class=conf_class)
         img = detector.draw_boxes(img, df)
         return img
 
-    @staticmethod
-    def img_to_base64(img):
+    def object_track(self, img, conf_th=0.3, conf_class=[]):
+        output = self.detector.prediction(img)
+        df = self.detector.filter_prediction(output, img, conf_th=conf_th, conf_class=conf_class)
+        img = detector.draw_boxes(img, df)
+        boxes = df[['x1', 'y1', 'x2', 'y2']].values
+        objects = self.ct.update(boxes)
+        if len(boxes) > 0 and (df['class_name'].str.contains('person').any()):
+            for (objectID, centroid) in objects.items():
+                text = "ID {}".format(objectID)
+                cv2.putText(img, text, (centroid[0] - 10, centroid[1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                cv2.circle(img, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+        return img
+
+    def img_to_base64(self, img):
         """encode as a jpeg image and return it"""
         buffer = cv2.imencode('.jpg', img)[1].tobytes()
         jpg_as_text = base64.b64encode(buffer)
@@ -79,8 +104,8 @@ def CaptureContinous(self):
         with PiRGBArray(camera, size=(WIDTH, HEIGHT)) as output:
             camera.capture(output, 'bgr', resize=(WIDTH, HEIGHT))
             image = output.array
-            output = detector.prediction(image)
-            df = detector.filter_prediction(output, image)
+            result = detector.prediction(image)
+            df = detector.filter_prediction(result, image)
             if len(df) > 0:
                 if (df['class_name']
                         .str
@@ -99,6 +124,46 @@ def CaptureContinous(self):
                             )
                     cv2.imwrite(filename_output, image)
 
+@celery.task(bind=True)
+def ObjectTracking(self):
+    detector = Detector()
+    myiter = glob.iglob(os.path.join(IMAGE_FOLDER, '**', '*.jpg'),
+                        recursive=True)
+    newdict = reduce(lambda a, b: reduce_tracking(a,b), myiter, dict())
+    startID = max(map(int, newdict.keys()), default=0) + 1
+    ct = CentroidTracker(startID=startID)
+    with PiCamera() as camera:
+        camera.resolution = (1280, 960)  # twice height and widht
+        camera.rotation = 180
+        camera.framerate = 10
+        with PiRGBArray(camera, size=(WIDTH, HEIGHT)) as output:
+            while True:
+                camera.capture(output, 'bgr', resize=(WIDTH, HEIGHT))
+                img = output.array
+                result = detector.prediction(img)
+                df = detector.filter_prediction(result, img)
+                img = detector.draw_boxes(img, df)
+                boxes = df[['x1', 'y1', 'x2', 'y2']].values
+                previous_object_ID = ct.nextObjectID
+                objects = ct.update(boxes)
+                if len(boxes) > 0 and (df['class_name'].str.contains('person').any()) and previous_object_ID in list(objects.keys()):
+                    for (objectID, centroid) in objects.items():
+                        text = "ID {}".format(objectID)
+                        cv2.putText(img, text, (centroid[0] - 10, centroid[1] - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                        cv2.circle(img, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+
+                    day = datetime.now().strftime("%Y%m%d")
+                    directory = os.path.join(IMAGE_FOLDER, 'pi', day)
+                    if not os.path.exists(directory):
+                        os.makedirs(directory)
+                    ids = "-".join(list([str(i) for i in objects.keys()]))
+                    hour = datetime.now().strftime("%H%M%S")
+                    filename_output = os.path.join(
+                            directory, "{}_person_{}_.jpg".format(hour, ids)
+                            )
+                    cv2.imwrite(filename_output, img)
+                time.sleep(0.300)
 
 if __name__ == '__main__':
     CaptureContinous()
